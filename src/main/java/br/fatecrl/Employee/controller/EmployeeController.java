250c83ca2f855717aeb54fc45224d0d49f508d47
JPedro759 <joaopedromeloo03@gmail.com>
2023-05-27 23:19:54 -0300

    First commit

--- src/main/java/br/fatecrl/Employee/controller/EmployeeController.java
+++ src/main/java/br/fatecrl/Employee/controller/EmployeeController.java
@@ -17,7 +17,7 @@ public class EmployeeController {
 
     public EmployeeController() {
         employees.add(new Employee(001, "Robert L. Stringer", "Developer FullStack", "Software development", 9859.57));
-        employees.add(new Employee(002, "Francis R. Schott", "Designer UX/UI", "Design and User Experience", 6897.54));
+        employees.add(new Employee(002, "Francis Schott", "Designer UX/UI", "Design and User Experience", 6897.54));
         employees.add(new Employee(003, "Simon S. Richards", "Support analyst", "Technical Support", 4859.27));
         employees.add(new Employee(004, "Kathy G. Hoff", "HR Manager", "Human Resources", 7059.44));
         employees.add(new Employee(005, "Mark A. Garmon", "Data Scientist", "Project Management", 8250.15));
